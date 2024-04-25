f083ecbc6eb4a263b886c5865d3aa90ee68b6286
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-04-24 21:17:10 -0500

    archivo JS

---
+++ pokemon.js
@@ -0,0 +1,6 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
